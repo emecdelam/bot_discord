03ecf30484c133c16526b4508618ee47eda8a209
Kaporos <theo@daron.be>
2024-01-22 15:32:28 +0100

    multiple prompts

--- commands/streak.py
+++ commands/streak.py
@@ -10,6 +10,36 @@ import pickle
 from client import MyClient
 from logging_system import log__,Level
 from providers.openai import ask_chat
+import random
+
+prompts = [
+"""I want you to generate a daily message for my users to ask them if they did their tasks (called TP) and i want you to end the message with sarcastic science/math fun fact/joke.
+Speak to them using some emotes (not too much) and using a familiar vocabulary. just do your best and be creative ! It will give us motivation
+Address to us using little insultant names to touch our ego ! (we are engineers)
+The message is a discord message and have to be short. Go strait to the point. 
+Please. Don't be nice to us. Threaten us to fail ! Call us with bad names.
+Remind them they will have to pay if they don't finish. You can also talk about the leaderboard (optionnal)
+Don't be kind. Be aggressive, insultant. We fucked your mother. Call us weak, little, nobrainer, whatever you want !
+Your message must be the shortest possible""", 
+
+"""
+I want you to generate a daily message for my users to ask them if they did their tasks (called TP). I want you to end your messae with an happy scientific/math quote !
+You can use emotes and you shall be a bright person ! Put some love in their lifes (you are a discord bot).
+
+(Your message must be the shortest possible)    
+""", 
+
+"""
+I want you to generate a daily message for my users to ask them if they did their tasks (called TP). I want you to end your message with a funny joke !
+
+(Your message must be the shortest possible)    
+Talk to us like you are an old student, say things like "when i was your age" (don't litteraly, adapt, but you get the idea !)
+
+"""
+
+
+)
+]
 
 class Streak(BotFeature):
     times = [time(hour = 0,minute = 0,tzinfo = timezone.utc)]
@@ -107,17 +137,7 @@ class Streak(BotFeature):
         if (datetime.now().weekday() > 4):
             return
 
-        content = ask_chat("""I want you to generate a daily message for my users to ask them if they did their tasks (called TP) and i want you to end the message with science/math fun fact/joke.
-
-Speak to them using some emotes (not too much) and using a familiar vocabulary. just do your best and be creative ! It will give us motivation
-Address to us using little insultant names to touch our ego ! (we are engineers)
-The message is a discord message and have to be short. Go strait to the point. 
-Please. Don't be nice to us. Threaten us to fail ! Call us with bad names.
-Remind them they will have to pay if they don't finish. You can also talk about the leaderboard (optionnal)
-Don't be kind. Be aggressive, insultant. We fucked your mother. Call us weak, little, nobrainer, whatever you want !
-
-Your message must be the shortest possible""")
-        
+        content = ask_chat(random.choice(prompts))        
         message:Message = await self.channel.send(content)
         self.messages[message.id] = {}
         for member in self.channel.guild.members:
