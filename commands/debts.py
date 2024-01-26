e95e28faa2fd9ce46a5ebb2cc7821cb42c85b57a
Emec Delam <kiwi.bp.surviv@gmail.com>
2024-01-26 15:42:45 +0100

    reformating debts class, adding getter methods

--- commands/debts.py
+++ commands/debts.py
@@ -1,19 +1,23 @@
 from typing import Dict
 
-from logging_system import log__,Level
 import pickle
 
+
 class Debt:
-    def __init__(self,user_id:int):
-        self.paid:int = 0
-        self.debt:int = 0
-        self.user_id:int = user_id
-    async def add_debt(self,amount:float) -> None:
+    def __init__(self,user_id: int):
+        self.paid: int = 0
+        self.debt: int = 0
+        self.user_id: int = user_id
+
+    async def add_debt(self,amount: float) -> None:
         self.debt += amount
-    async def remove_debt(self,amount:float) -> None:
+
+    async def remove_debt(self,amount: float) -> None:
         self.paid += amount
+
     async def due(self) -> float:
-        return self.debt -self.paid
+        return self.debt - self.paid
+
     async def update_debt(self) -> None:
         messages = pickle.load(open("db\\messages.p","rb"))
         score = {}
@@ -29,6 +33,7 @@ class Debt:
                     score[self.user_id] = 5
         if self.user_id in score.keys():
             self.debt = score[self.user_id]
+
     async def get_debt(self) -> float:
         await self.update_debt()
         return self.debt
@@ -36,10 +41,20 @@ class Debt:
 
 class Debts:
     def __init__(self):
-        self.debts:Dict[int,Debt] = {}
-    def add_debt(self,debt:Debt):
+        self.debts: Dict[int,Debt] = {}
+
+    def add_debt(self,debt: Debt) -> None:
         self.debts[debt.user_id] = debt
-async def save_debts(debts:Debts):
+
+    def get_debt(self,id: int) -> Debt:
+        if id in self.debts.keys():
+            return self.debts[id]
+        return Debt(id)
+
+
+async def save_debts(debts: Debts) -> None:
     pickle.dump(debts,open("db\\debts.p","wb"))
-async def read_debts():
+
+
+async def read_debts() -> Debts:
     return pickle.load(open("db\\debts.p","wb"))
