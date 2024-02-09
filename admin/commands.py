09ed8060cbb12e8c8786ff0e8fc49f3f52784c7e
Emec Delam <kiwi.bp.surviv@gmail.com>
2024-02-09 19:25:22 +0100

    add admin power

--- admin/commands.py
+++ admin/commands.py
@@ -1,3 +1,5 @@
+import json
+import pickle
 from datetime import datetime
 
 from aioconsole import ainput
@@ -8,9 +10,28 @@ async def clean_log(*args):
     with open('error.log','w') as log_file:
         log_file.write('')
     await log__("Log file cleaned.",Level.DEBUG,Colors.lightcyan)
+async def print_db(*args):
+    """Writes the messages in the db"""
+    messages = pickle.load(open("db\\messages.p","rb"))
+    with open("admin/db.json","w") as file:
+        json.dump(messages,file,indent = 4)
+    await log__("Message writted in admin/db",Level.DEBUG,Colors.lightcyan)
+async def switch_db(msg_id,user_id,*args):
+    """Switch the state of a user in the messages db list"""
+    messages = pickle.load(open("db\\messages.p","rb"))
+    if msg_id in messages.keys():
+        if user_id in messages[msg_id].keys():
+            messages[msg_id][user_id] = not messages[msg_id][user_id]
+            pickle.dump(messages,open("db\\messages.p","rb"))
+        else:
+            await log__("User not in db",Level.DEBUG,Colors.lightcyan)
+    else:
+        await log__("Message not in db",Level.DEBUG,Colors.lightcyan)
 async def command_handler(client):
     commands_dic = {
         'clean_logs':clean_log,
+        'switch_db':switch_db,
+        'print_db':print_db
     }
     while True:
         command = await ainput(f'{Colors.reset}{Colors.bold}[{str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")).ljust(19)}] [{str(Level.INFO).ljust(8)}] Enter a command (or help): {Colors.reset}\n')
